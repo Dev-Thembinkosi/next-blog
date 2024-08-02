ab81305ff8a0d69119ca9087af1f9cb16a9e5339
TManhique <manhiquet0@gmail.com>
2024-08-02 09:52:54 +0200

    Home cover completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import { allBlogs } from "contentlayer/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
+import { FeaturePosts } from "../components/Home/FeaturePosts";
 
 export default function Home() {
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs}/>
+      <FeaturePosts blogs={allBlogs}/>
     </main>
   )
 }
