bb9c08f6c332a4cef059d25c6d9dbe2e2e4e6627
TManhique <manhiquet0@gmail.com>
2024-08-06 09:23:44 +0200

    Recent Post completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { allBlogs } from "contentlayer/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
 import { FeaturePosts } from "../components/Home/FeaturePosts";
+import { RecentPosts } from "../components/Home/RecentPosts";
 
 export default function Home() {
 
@@ -10,6 +11,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs}/>
       <FeaturePosts blogs={allBlogs}/>
+      <RecentPosts blogs={allBlogs} />
     </main>
   )
 }
