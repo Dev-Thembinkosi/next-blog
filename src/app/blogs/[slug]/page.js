d8f3405f3b595c839e0532ed10059f3127de3c20
TManhique <manhiquet0@gmail.com>
2024-08-19 10:10:17 +0200

    toc completed

--- src/app/blogs/[slug]/page.js
+++ src/app/blogs/[slug]/page.js
@@ -49,11 +49,14 @@ export default function BlogPage({ params }) {
                         data-level={heading.level}
                         className="data-[level=two]:pl-0 data-[level=two]:pt-2
                           data-[level=two]:border-t border-solid border-dark/40
-                          data-[level=three]:pl-4
+                          data-[level=three]:pl-6
                           flex items-center justify-start
                         "
                       >
-                        <span>{heading.text}</span>
+                        {
+                          heading.level === "three" ? <span className="flex w-1 h-1 rounded-full bg-dark mr-2">&nbsp;</span> : null
+                        }
+                        <span  className="hover:underline">{heading.text}</span>
                       </a>
                     </li>
                   );
