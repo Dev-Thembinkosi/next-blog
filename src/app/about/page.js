85d13eeb38e2c0d2dc6d6de6a0044a793bbb2cd4
TManhique <manhiquet0@gmail.com>
2024-09-02 09:53:44 +0200

    profile cover component completed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,9 +1,11 @@
 import { AboutCoverSection } from "@/src/components/About/AboutCoverSection";
+import { Skills } from "@/src/components/About/Skills";
 
 export default function about(){
     return(
         <>
             <AboutCoverSection />
+            <Skills />
         </>
     )
 }
---
+++ src/components/About/Skills.js
@@ -0,0 +1,24 @@
+
+
+const SkillList = [
+    "next.js",
+    "tailwind css",
+    "figma",
+    "javaScript",
+    "web design",
+    "Gatsby.js",
+    "strapi",
+    "firebase",
+    "generative AI",
+    "wireframing",
+    "SEO",
+    "framer motion",
+    "sanity",
+  ];
+
+export const Skills = () => {
+  return (
+    <section className="w-full flex flex-col p-20 border-b-2 border-solid border-dark text-dark">
+        <span className="">I'm comfortable in...</span>
+
+    </section
