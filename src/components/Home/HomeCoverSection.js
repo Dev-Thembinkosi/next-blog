8d3e7821905a714adfce18b5ea0b3ad453d6c13c
TManhique <manhiquet0@gmail.com>
2024-09-11 23:51:33 +0200

    InsightRoll component completed

--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -8,7 +8,7 @@ import { slug } from "github-slugger";
 function HomeCoverSection({blogs}) {
 
     const sortedBlogs = sortBlogs(blogs);
-    const blog = sortedBlogs[1];
+    const blog = sortedBlogs[0];
 
 
 
