6a1abfcb9f29df5316092b3294567a8a42dffb64
TManhique <manhiquet0@gmail.com>
2024-12-11 11:02:10 +0200

    Dark mode error fix

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -4,11 +4,11 @@ import profileImg from "@/public/profile-img.png"
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center text-dark">
-        <div className="w-16 rounded-full overflow-hidden border border-solid border-dark mr-4">
+    <Link href="/" className="flex items-center text-dark dark:text-light">
+        <div className="w-16 rounded-full overflow-hidden border border-solid border-darkdark:border-light mr-4">
             <Image src={profileImg} alt="log" className="w-full h-auto rounded-full"/>
         </div>
-        <span className="font-bold text-xl text-gray-500">T /DevLogs </span>
+        <span className="font-bold text-xl dark:font-semibold text-gray-500">T /DevLogs </span>
     </Link>
   )
 }
