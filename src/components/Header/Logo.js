030bb9e985c95be29143ecfeb93cde654ffda437
TManhique <manhiquet0@gmail.com>
2024-12-14 12:37:47 +0200

    Toggle fix

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,10 +5,10 @@ import profileImg from "@/public/profile-img.png"
 const Logo = () => {
   return (
     <Link href="/" className="flex items-center text-dark dark:text-light">
-        <div className="w-16 rounded-full overflow-hidden border border-solid border-darkdark:border-light mr-4">
+        <div className="w-12 md:w-16 rounded-full overflow-hidden  mr-2 md:mr-4">
             <Image src={profileImg} alt="log" className="w-full h-auto rounded-full"/>
         </div>
-        <span className="font-bold text-xl dark:font-semibold text-gray-500">T /DevLogs </span>
+        <span className="font-bold text-lg md:text-xl dark:font-semibold text-gray-500"> T Dev </span>
     </Link>
   )
 }
